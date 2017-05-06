cf5da721b42773edaf39af26c98e15607eebec9a
infinite chill <infinitechillzone@gmail.com>
2017-05-06 00:16:23 -0500

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 'use strict';
+
 var Alexa = require('alexa-sdk');
-
-//=========================================================================================================================================
-//TODO: The items below this comment need your attention.
-//=========================================================================================================================================
-
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
-//Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
 var APP_ID = undefined;
-
 var SKILL_NAME = "daves jokes";
 var GET_FACT_MESSAGE = "This negro said: ";
-var HELP_MESSAGE = "You can say tell me a Chappelle joke, or, you can say exit... What can I help you with?";
+var HELP_MESSAGE = "You can say tell me a dave joke, or, you can say exit... What can I help you with?";
 var HELP_REPROMPT = "What can I help you with?";
 var STOP_MESSAGE = "Goodbye!";
 
-//=========================================================================================================================================
-//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
-//=========================================================================================================================================
 var data = [
     "Hey, baby! Stop selling weed, all right, you've got your whole life ahead of you. He goes, Fuck you, nigga. I got kids to feed!",
     "All I'll say about Elian is thank God he's Cuban. 'Cause if he was Haitian you'd've never heard about his ass. If Elian Gonzales was Elian Mumumbo from Haiti, they would've pushed that little rubber tube right back in the water. Sorry little fella, all full. Good luck!",
@@ -34,9 +24,6 @@ var data = [
     "Like, see, I'd never vote for George Bush Junior, but I don't know anything about his politics. All I know about that George Bush Junior is that that guy sniffed cocaine. That's right. Now, listen, we can't have that shit in the White House. That may be fine for a mayor; but goddammit, not the White House! The stakes are too high. He'd be sellin' nuclear secrets for 20 or 30 dollars and shit."
 ];
 
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.  
-//=========================================================================================================================================
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
